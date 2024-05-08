6de9f2932c74a486ef7793d069290bfbb8a48376
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-05-08 11:00:01 -0300

    feat: adicionando a funcao de consultar os usuarios pelo nome

--- src/func/usuario/cadastrarUsuario.py
+++ src/func/usuario/cadastrarUsuario.py
@@ -2,9 +2,14 @@ from src.func.criarChave import criarChave
 from src.data.redis.func.set import set
 from src.utils.usuarioParaJson import usuarioParaJson
 from src.func.usuario.criarUsuario import criarUsuario
+from src.func.verificarChaveExistente import verificarChaveExistente
 
 def cadastrarUsuario():
     chave = criarChave("usuario")
+    if (verificarChaveExistente("usuario", chave)):
+        print("\nUsuário já cadastrado!")
+        input()
+        return
     usuario = criarUsuario()
     try:
         set(chave, usuarioParaJson(usuario, chave))
--- src/func/usuario/consultarUsuario.py
+++ src/func/usuario/consultarUsuario.py
@@ -1,17 +1,36 @@
 from src.func.buscarDados import BuscarDados
 from src.func.usuario.listarUsuarios import listarUsuarios
 from src.func.criarChave import criarChave
+from src.data.redis.func.get import get
+from src.func.verificarChaveExistente import verificarChaveExistente
 
 def consultarUsuario():
-    chave = str(input("Digite a chave do usuário (deixe vazio para listar todos): "))
+    # chave = str(input("Digite a chave do usuário (deixe vazio para listar todos): "))
+    # try:
+    #     print()
+    #     if chave == "":
+    #         usuarios = BuscarDados("usuario")
+    #         listarUsuarios(usuarios)
+    #     else:
+    #         chave = criarChave("usuario", chave)
+    #         if (verificarChaveExistente("usuario", chave)):
+    #             usuario = get(chave)
+    #             listarUsuarios([usuario])
+
+    nome = str(input("Digite o nome do usuário (deixe vazio para listar todos): "))
     try:
         print()
-        if chave == "":
+        if nome == "":
             usuarios = BuscarDados("usuario")
             listarUsuarios(usuarios)
         else:
-            chave = criarChave("usuario", chave)
-            print(f"buscando usuario com a chave: {chave}") # FAZER A FUNCAO DE BUSCAR COM UMA CHAVE ESPECIFICA
+            usuarios = BuscarDados("usuario")
+            usuarios = [usuario for usuario in usuarios if usuario["nome"] == nome]
+            if usuarios == []:
+                print("Usuário não encontrado")
+                input()
+                return
+            listarUsuarios(usuarios)
     except Exception as e:
         print(f"\nErro ao buscar usuário: {e}")
         input()
