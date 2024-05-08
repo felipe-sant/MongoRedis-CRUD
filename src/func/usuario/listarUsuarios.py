7b63fcc347417bb243ce1e67d1d562470637aa75
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-05-08 08:58:28 -0300

    feat: adicionando um print caso nao tenha usuarios cadastrados no banco

--- src/func/usuario/listarUsuarios.py
+++ src/func/usuario/listarUsuarios.py
@@ -1,14 +1,18 @@
 from src.utils.jsonParaUsuario import jsonParaUsuario
 
 def listarUsuarios(usuarios):
-    contador = 0
-    total = len(usuarios)
-    for elemento in usuarios:
-        contador += 1
-        usuario = jsonParaUsuario(elemento)
-        print(f"- usuário - ({contador}/{total}) -")
-        if "chave" in elemento:
-            chave = elemento["chave"].replace("usuario@", "")
-            print(f"chave: {chave}")
-        usuario.mostrar()
+    if usuarios == []:
+        print("Não há usuários cadastrados")
         input()
+    else:
+        contador = 0
+        total = len(usuarios)
+        for elemento in usuarios:
+            contador += 1
+            usuario = jsonParaUsuario(elemento)
+            print(f"- usuário - ({contador}/{total}) -")
+            if "chave" in elemento:
+                chave = elemento["chave"].replace("usuario@", "")
+                print(f"chave: {chave}")
+            usuario.mostrar()
+            input()
