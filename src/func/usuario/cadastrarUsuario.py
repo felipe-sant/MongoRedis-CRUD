b8b358787ad3ae655277560840b33ddd16083c8f
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-05-08 07:56:00 -0300

    chore: adicionando opcao de erro ao cadastrar e listar usuario

--- src/func/usuario/cadastrarUsuario.py
+++ src/func/usuario/cadastrarUsuario.py
@@ -4,12 +4,13 @@ from src.utils.usuarioParaJson import usuarioParaJson
 from src.func.usuario.criarUsuario import criarUsuario
 
 def cadastrarUsuario():
+    chave = criarChave("usuario")
+    usuario = criarUsuario()
     try:
-        chave = criarChave("usuario")
-        usuario = criarUsuario()
         set(chave, usuarioParaJson(usuario, chave))
         print("\nUsuário cadastrado com sucesso!")
         input()
     except Exception as e:
         print(f"\nErro ao cadastrar usuário: {e}")
+        input()
     
--- src/func/usuario/consultarUsuario.py
+++ src/func/usuario/consultarUsuario.py
@@ -3,11 +3,15 @@ from src.func.usuario.listarUsuarios import listarUsuarios
 from src.func.criarChave import criarChave
 
 def consultarUsuario():
-    chave = str(input("Digite a chave do usuario (deixe vazio para listar todos): "))
-    print()
-    if chave == "":
-        usuarios = BuscarDados("usuario")
-        listarUsuarios(usuarios)
-    else:
-        chave = criarChave("usuario", chave)
-        print(f"buscando usuario com a chave: {chave}")
+    chave = str(input("Digite a chave do usuário (deixe vazio para listar todos): "))
+    try:
+        print()
+        if chave == "":
+            usuarios = BuscarDados("usuario")
+            listarUsuarios(usuarios)
+        else:
+            chave = criarChave("usuario", chave)
+            print(f"buscando usuario com a chave: {chave}")
+    except Exception as e:
+        print(f"Erro ao buscar usuário: {e}")
+        input()
